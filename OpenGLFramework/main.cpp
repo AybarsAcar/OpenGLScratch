44c2393dd1847e7ff1f320797b09c1d469080a1a
Aybars Acar <aybars.dev@gmail.com>
2021-01-21 22:31:53 +1100

    Initial Commit

---
+++ OpenGLFramework/main.cpp
@@ -0,0 +1,14 @@
+//
+//  main.cpp
+//  OpenGLFramework
+//
+//  Created by Aybars Acar on 21/1/21.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+  // insert code here...
+  std::cout << "Hello, World!\n";
+  return 0;
+}
